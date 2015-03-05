8c4aecea38cc8c2450564c2ecef14f015b2ec5be
Daniel Howe <daniel@rednoise.org>
2015-03-05 17:29:00 +0800

    autocommit

--- autocommit.sh
+++ autocommit.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-git pull
+git fetch upstream
+git checkout master
+git merge upstream/master
 TIMESTAMP=`date +%s`
 sed -i "" "s/##[0-9][0-9]*##/##${TIMESTAMP}##/g" last_build
 git commit -a -m 'autocommit'
