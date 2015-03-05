638f9fc718eb912e49bb1d680a765db2f7bac7e6
travis-auto <travis-auto@outlook.com>
2015-03-05 08:47:58 -0500

    autocommit-1425563278

--- autocommit-fork.sh
+++ autocommit-fork.sh
@@ -10,11 +10,9 @@ git config user.email "travis-auto@outlook.com"
 git fetch upstream
 git checkout master
 git merge upstream/master
-git pull
 TIMESTAMP=`date +%s`
 sed -i "" "s/##[0-9][0-9]*##/##${TIMESTAMP}##/g" last_build
-git status
-git commit -a -m 'autocommit-ta'
-git push   
-git push -u upstream master
+git commit -a -m "autocommit-${TIMESTAMP}"
+#git push   
+#git push -u upstream master
 exit
